f490bfee246c2a5eb36ee86bf34173ff1574e336
Damian Moser <damian.d.moser@gmail.com>
2026-01-22 21:08:44 +0100

    Properly import core.nix

--- core.nix
+++ core.nix
@@ -1,7 +1,15 @@
+{ config, lib, ... }:
+
 {
   imports = [
     ./options.nix
     ./services
   ];
+
+  system.activationScripts.create-neo-volumes = {
+    text = "mkdir -p ${builtins.concatStringsSep " " (builtins.attrValues config.neo.volumes)}";
+    deps = [ ];
+  };
+
   system.stateVersion = "24.11";
 }
