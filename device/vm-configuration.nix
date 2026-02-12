0f1e9c9e474f155d0e58960fd5b10740d1fc9916
Damian Moser <damian.d.moser@gmail.com>
2026-02-12 19:42:48 +0100

    Enable dbus

--- device/vm-configuration.nix
+++ device/vm-configuration.nix
@@ -2,10 +2,10 @@
   config,
   lib,
   ...
-}: {
+}:
+{
   users.allowNoPasswordLogin = true;
   users.mutableUsers = false;
-  services.dbus.enable = lib.mkForce false;
   # Minimal boot and filesystem config for hardware/VM deployment
   # boot.loader.grub.device = "/dev/sda";
   # fileSystems."/".device = "/dev/sda1";
