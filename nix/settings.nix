7b43724945df44cb3348c2f07d390f3e0817a919
Damian Moser <damian.d.moser@gmail.com>
2026-03-12 08:24:04 +0100

    Make default settings only default

--- nix/settings.nix
+++ nix/settings.nix
@@ -6,16 +6,16 @@
     pkgs,
     ...
   }: {
-    neo.ssh.authorizedKeys = [
+    neo.ssh.authorizedKeys = lib.mkDefault [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9fFR8aERyyLjqI0aG08BXmSaMemGXK4WK8bLy7Nzmc development"
     ];
 
     neo.volumes = {
-      root = "/var/neo";
-      appdata = "/var/neo/DATA/AppData";
-      data = "/var/neo/DATA";
-      media = "/var/neo/DATA/Media";
-      documents = "/var/neo/DATA/Documents";
+      root = lib.mkDefault "/var/neo";
+      appdata = lib.mkDefault "/var/neo/DATA/AppData";
+      data = lib.mkDefault "/var/neo/DATA";
+      media = lib.mkDefault "/var/neo/DATA/Media";
+      documents = lib.mkDefault "/var/neo/DATA/Documents";
     };
   };
 }
