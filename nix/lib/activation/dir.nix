21cd5aff45910b61b15b0b602e14e5f386a1f768
Damian Moser <damian.d.moser@gmail.com>
2026-03-18 08:45:45 +0100

    lib: make dir activation only if it not exists

--- nix/lib/activation/dir.nix
+++ nix/lib/activation/dir.nix
@@ -1,16 +1,22 @@
-{...}: {
+{ ... }:
+{
   libExtensions.activate-dir = {
     neo = {
-      mkActivationScriptForDir = config: {
-        dirPath,
-        mode ? "0755",
-        user ? toString config.neo.uid,
-        group ? toString config.neo.gid,
-      }: ''
-        mkdir -p ${dirPath}
-        chown -R ${user}:${group} ${dirPath}
-        chmod -R ${mode} ${dirPath}
-      '';
+      mkActivationScriptForDir =
+        config:
+        {
+          dirPath,
+          mode ? "0755",
+          user ? toString config.neo.uid,
+          group ? toString config.neo.gid,
+        }:
+        ''
+          if [ ! -e ${dirPath} ]; then
+            mkdir -p ${dirPath}
+            chown ${user}:${group} ${dirPath}
+            chmod ${mode} ${dirPath}
+          fi
+        '';
     };
   };
 }
