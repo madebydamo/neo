b12596374f904fcec0df702a7c8de81291fb629e
Damian Moser <damian.d.moser@gmail.com>
2026-03-19 13:46:03 +0100

    lib: format dir activation

--- nix/lib/activation/dir.nix
+++ nix/lib/activation/dir.nix
@@ -1,22 +1,18 @@
-{ ... }:
-{
+{...}: {
   libExtensions.activate-dir = {
     neo = {
-      mkActivationScriptForDir =
-        config:
-        {
-          dirPath,
-          mode ? "0755",
-          user ? toString config.neo.uid,
-          group ? toString config.neo.gid,
-        }:
-        ''
-          if [ ! -e ${dirPath} ]; then
-            mkdir -p ${dirPath}
-            chown ${user}:${group} ${dirPath}
-            chmod ${mode} ${dirPath}
-          fi
-        '';
+      mkActivationScriptForDir = config: {
+        dirPath,
+        mode ? "0755",
+        user ? toString config.neo.uid,
+        group ? toString config.neo.gid,
+      }: ''
+        if [ ! -e ${dirPath} ]; then
+          mkdir -p ${dirPath}
+          chown ${user}:${group} ${dirPath}
+          chmod ${mode} ${dirPath}
+        fi
+      '';
     };
   };
 }
