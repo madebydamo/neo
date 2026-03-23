34b1feb14aa6a4a7d62f80e38660244c2d12820f
Damian Moser <damian.d.moser@gmail.com>
2026-03-23 20:13:18 +0100

    bootstrap: initial try

---
+++ nix/modules/bootstrap/homeserver-config/option.nix
@@ -0,0 +1,66 @@
+{...}: {
+  flake.modules.nixos.homeserver-config-option = {
+    config,
+    lib,
+    ...
+  }: let
+    inherit
+      (lib)
+      types
+      mkOption
+      mkEnableOption
+      mdDoc
+      ;
+  in {
+    options.neo.homeserverConfig = mkOption {
+      type = types.submodule (
+        {...}: {
+          options = {
+            enabled = mkEnableOption (mdDoc "Bootstrap homeserver configuration");
+            repoUrl = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = mdDoc "Git repository URL for the homeserver configuration";
+            };
+            configPath = mkOption {
+              type = types.str;
+              default = "${config.neo.volumes.appdata}/configuration";
+              description = mdDoc "Path to the homeserver configuration repository";
+            };
+            bootstrapEnable = mkEnableOption (mdDoc "Bootstrap the git repository if .git is missing");
+            bootstrapUser = mkOption {
+              type = types.str;
+              default = "root";
+              description = mdDoc "User to run the bootstrap service as";
+            };
+            safeDirectoryEnable = mkEnableOption (mdDoc "Configure git safe.directory for configPath");
+            pluginGeneratorEnable = mkEnableOption (mdDoc "Generate plugins.toml from Nix config");
+            pluginGeneratorPlugins = mkOption {
+              type = types.attrs;
+              default = {};
+              description = mdDoc "Attrs converted to plugins.toml";
+            };
+            rebuildEnable = mkEnableOption (mdDoc "Enable rebuild timer and service");
+            rebuildTimer = mkOption {
+              type = types.str;
+              default = "hourly";
+              description = mdDoc "systemd timer OnCalendar value";
+            };
+            rebuildBranchFormat = mkOption {
+              type = types.str;
+              default = "%Y%m%d-%H%M%S";
+              description = mdDoc "printf format for branch name";
+            };
+            rebuildBtrfsSubvol = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = mdDoc "Btrfs subvol to snapshot before rebuild";
+            };
+          };
+        }
+      );
+      default = {};
+      description = mdDoc "Homeserver bootstrap configuration";
+    };
+  };
+}
