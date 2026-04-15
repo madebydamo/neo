b3844aabaaa2d6982ae3d8b1d70f499f0dc11c5a
Damian Moser <damian.d.moser@gmail.com>
2026-04-15 15:57:35 +0200

    vm: define default filesystem options to make flake check pass

--- nix/output/device/vm-configuration.nix
+++ nix/output/device/vm-configuration.nix
@@ -9,8 +9,8 @@
     users.allowNoPasswordLogin = true;
     users.mutableUsers = false;
     # boot.loader.grub.device = "/dev/vda";
-    # fileSystems."/".device = "/dev/vda1";
-    # fileSystems."/".fsType = "ext4";
+    fileSystems."/".device = lib.mkDefault "/dev/vda1";
+    fileSystems."/".fsType = lib.mkDefault "ext4";
     virtualisation = {
       diskSize = 1024000;
       docker.enable = true;
