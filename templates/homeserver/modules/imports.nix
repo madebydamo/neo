b5446ee96e7c04947a4125f8fcf5b3faeed9b027
Damian Moser <damian.d.moser@gmail.com>
2026-06-04 11:00:49 +0200

    template: generate properly formated flake.nix on write-flake

--- templates/homeserver/modules/imports.nix
+++ templates/homeserver/modules/imports.nix
@@ -2,4 +2,5 @@
   imports = [
     inputs.flake-file.flakeModules.dendritic
   ];
+  flake-file.outputs = "inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)";
 }
